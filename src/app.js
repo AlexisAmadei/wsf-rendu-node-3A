0ec2e68f37cf44bd66052408ba80a3ea6aebc005
Alexis Amadei <alexisamadei@gmail.com>
2024-06-21 03:38:13 +0200

    add product use in app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+
 const userRoutes = require('./userBackend/userRoutes');
 const authRoutes = require('./authBackend/authRoutes');
+const productRoutes = require('./productBackend/productRoutes');
 // const contentRoutes = require('./contentBackend/contentRoutes');
 
 app.use(express.json());
 app.use('/usr', userRoutes);
 app.use('/auth', authRoutes);
+app.use('/product', productRoutes);
 // app.use('/content', contentRoutes);
 
 const PORT = 80;
